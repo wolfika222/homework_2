c8a3fe5a2713d8a9b65089932778217a9b872eb6
wolfika222 <farkas0528@gmail.com>
2017-03-26 21:04:10 +0200

    #3

--- src/hu/helixlab/Main.java
+++ src/hu/helixlab/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         CalculatorService calculatorservice = new CalculatorService();
+        CalculationHelper calculationHelper = new CalculationHelper();
 
         System.out.println(calculatorservice.isItEvent(12));
-        System.out.print(calculatorservice.calculate(5,2,'*'));
+        System.out.print(calculatorservice.calculate(5,2,'*') + "\n");
+        calculationHelper.input();
+        System.out.println("Eredmény: " + calculatorservice.calculate(calculationHelper.a, calculationHelper.b, calculationHelper.op));
     }
 }
